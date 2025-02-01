935c04e0aff1b549354d14a26ed0b22546151da1
jennyfer04 <124286825+jennyfer04@users.noreply.github.com>
2025-02-01 22:15:20 +0200

    users

--- Dump20250131/ems_db_users.sql
+++ Dump20250131/ems_db_users.sql
@@ -30,7 +30,6 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `isSupplier` tinyint(1) DEFAULT '0',
   `ishost` tinyint(1) DEFAULT '0',
-  `ratings` decimal(3,2) DEFAULT NULL,
   `cuisineId` int DEFAULT NULL,
   `employee_pin` int DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -46,7 +45,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (3,'John Doe','supplier@gmail.com','+96170000000','$2b$12$J/H49urnqixg4EK9FZoJpO7HhSkIg571goaFzwoqqs31r6kRLtani',1,0,NULL,1,NULL),(6,'Anthony hani Nemer','user@gmail.com','+96196181933092','$2b$12$sUTeJbguNInpHrUoTHqSQO8Bq0flgy5b6e5icaLhIKljMRL7Oj75q',0,0,NULL,NULL,NULL),(7,'Jenny Alpou','host@gmail.com','+96181933092','$2b$12$5ZBLQftH8SSodrmM45m0fO0AeJ4eDJdlJzRd9DOLygltIv3UVnaK2',0,1,NULL,NULL,22222),(8,'user','jennuser@gmail.com','+96179130353','$2b$12$QDcVWIRC4wRPYr9z6JCDZefSC/im6xZqH1xO/opjy3k00D1TJs0UW',0,0,NULL,NULL,NULL),(9,'host','jennhost@gmail.com','+96179130353','$2b$12$xaFu0xeB0sWXFiXFkOiw/.RzdWOXa5I.qZSRnGU7tMelGB4gjEIsm',0,1,NULL,NULL,123),(10,'supplier','jennsupplier@gmail.com','+96179130353','$2b$12$6h6EYhcVP5I3qKE4K9.IPefjkIokpxknDlqzNow6uH0rOWcqu1kNC',1,0,NULL,1,NULL),(27,'test2','test2@gmail.com','+9613000000','$2b$12$jRhCJBPEUTdJ1I0Y9UhqVuzzRZGzQQ3Dz0MNdhAsr/c9RFCB1JiQK',1,0,NULL,3,NULL);
+INSERT INTO `users` VALUES (3,'John Doe','supplier@gmail.com','+96170000000','$2b$12$J/H49urnqixg4EK9FZoJpO7HhSkIg571goaFzwoqqs31r6kRLtani',1,0,1,NULL),(6,'Anthony hani Nemer','user@gmail.com','+96196181933092','$2b$12$sUTeJbguNInpHrUoTHqSQO8Bq0flgy5b6e5icaLhIKljMRL7Oj75q',0,0,NULL,NULL),(7,'Jenny Alpou','host@gmail.com','+96181933092','$2b$12$5ZBLQftH8SSodrmM45m0fO0AeJ4eDJdlJzRd9DOLygltIv3UVnaK2',0,1,NULL,22222),(8,'user','jennuser@gmail.com','+96179130353','$2b$12$QDcVWIRC4wRPYr9z6JCDZefSC/im6xZqH1xO/opjy3k00D1TJs0UW',0,0,NULL,NULL),(9,'host','jennhost@gmail.com','+96179130353','$2b$12$xaFu0xeB0sWXFiXFkOiw/.RzdWOXa5I.qZSRnGU7tMelGB4gjEIsm',0,1,NULL,123),(10,'supplier','jennsupplier@gmail.com','+96179130353','$2b$12$6h6EYhcVP5I3qKE4K9.IPefjkIokpxknDlqzNow6uH0rOWcqu1kNC',1,0,1,NULL),(27,'test2','test2@gmail.com','+9613000000','$2b$12$jRhCJBPEUTdJ1I0Y9UhqVuzzRZGzQQ3Dz0MNdhAsr/c9RFCB1JiQK',1,0,3,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -59,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-01 22:08:22
+-- Dump completed on 2025-02-01 22:13:47
